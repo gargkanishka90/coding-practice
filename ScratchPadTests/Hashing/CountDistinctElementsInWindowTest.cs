6382b54de2850f292debd7403b556c99e6a12508
Kanishka Garg <kanishka.garg@insureon.com>
2016-12-06 13:17:11 -0600

    Count Distinct Elements in a Window of Size K and test

---
+++ ScratchPadTests/Hashing/CountDistinctElementsInWindowTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using ScratchPadTests.Hashing;
+
+namespace ScratchPad.Hashing
+{
+    public class CountDistinctElementsInWindowTest
+    {
+        [Test]
+        public void Count()
+        {
+            int[] nums = {1, 2, 1, 3, 4, 2, 3};
+            CountDistinctElementsInWindow.Count(nums, 4);
+        } 
+    }
+}
