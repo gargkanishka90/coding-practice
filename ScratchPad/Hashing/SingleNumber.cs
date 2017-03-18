d1cad545d1a9c3a5a1bf0cb789a9a30715d6fab8
Kanishka Garg <kanishka.garg@insureon.com>
2017-03-18 13:30:19 -0500

    push uncomitted changes

---
+++ ScratchPad/Hashing/SingleNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ScratchPad.Hashing
+{
+    public class SingleNumber
+    {
+        public static int Find(int[] numbers)
+        {
+            if (numbers.Length <= 2)
+            {
+                throw new Exception();
+            }
+
+            var result = numbers[0];
+            for(var i = 1; i < numbers.Length; i++)
+            {
+                result ^= numbers[i];
+            }
+            return result;
+        } 
+    }
+}
---
+++ ScratchPad/Tests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using ScratchPad.DynamicProgramming;
+
+namespace ScratchPad.Tests
+{
+    public class LongestIncreasingSubsequenceTests
+    {
+        [Test]
+        public void TestRecursiveLIS()
+        {
+            var lisCount = LongestIncreasingSubsequence.LISRecursive(new[] {10, 22, 9, 33, 21, 50, 41, 60});
+            Assert.IsTrue(lisCount == 5);
+
+            lisCount = LongestIncreasingSubsequence.LIS_DP(new[] { 10, 22, 9, 33, 21, 50, 41, 60 });
+            Assert.IsTrue(lisCount == 5);
+        } 
+    }
+}
