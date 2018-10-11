e562aca1009bbc2fde47e0a67d336440dfbbab72
Kanishka Garg <kanishka.garg@insureon.com>
2018-10-11 17:57:32 -0500

    add code for LeetCode 273. Integer to English Words

---
+++ ScratchPadTests/Tests/StringTests/IntegerToEnglishWordTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace ScratchPadTests.Tests.StringTests
+{
+    [TestFixture]
+    public class IntegerToEnglishWordTests
+    {
+        [Test]
+        public void Test01()
+        {
+            var instance = new IntegerToEnglishWords();
+
+            Assert.IsTrue(instance.NumberToWords(1).Contains("One"));
+            Assert.IsTrue(instance.NumberToWords(12).Contains("Twelve"));
+
+            Assert.IsTrue(instance.NumberToWords(75).Contains("Seventy Five"));
+            Assert.IsTrue(instance.NumberToWords(60).Contains("Sixty"));
+            Assert.IsTrue(instance.NumberToWords(88).Contains("Eighty Eight"));
+
+            Assert.IsTrue(instance.NumberToWords(100).Contains("One Hundred"));
+            Assert.IsTrue(instance.NumberToWords(500).Contains("Five Hundred"));
+            Assert.IsTrue(instance.NumberToWords(123).Contains("One Hundred Twenty Three"));
+            Assert.IsTrue(instance.NumberToWords(750).Contains("Seven Hundred Fifty"));
+
+            Assert.IsTrue(instance.NumberToWords(1234).Contains("One Thousand Two Hundred Thirty Four"));
+            Assert.IsTrue(instance.NumberToWords(12345).Contains("Twelve Thousand Three Hundred Forty Five"));
+            Assert.IsTrue(instance.NumberToWords(123456).Contains("One Hundred Twenty Three Thousand Four Hundred Fifty Six"));
+            Assert.IsTrue(instance.NumberToWords(1234567).Contains("One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"));
+            Assert.IsTrue(instance.NumberToWords(1234567891).Contains("One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"));
+        }
+    }
+}
---
+++ ScratchPadTests/Tests/StringTests/IntegerToEnglishWords.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchPadTests.Tests.StringTests
+{
+    public class IntegerToEnglishWords
+    {
+        public string NumberToWords(int input)
+        {
+            var map = new Dictionary<int, string>()
+            {
+                [1] = "One",
+                [2] = "Two",
+                [3] = "Three",
+                [4] = "Four",
+                [5] = "Five",
+                [6] = "Six",
+                [7] = "Seven",
+                [8] = "Eight",
+                [9] = "Nine",
+                [10] = "Ten",
+                [11] = "Eleven",
+                [12] = "Twelve",
+                [13] = "Thirteen",
+                [14] = "Fourteen",
+                [15] = "Fifteen",
+                [16] = "Sixteen",
+                [17] = "Seventeen",
+                [18] = "Eighteen",
+                [19] = "Nineteen",
+                [20] = "Twenty",
+                [30] = "Thirty",
+                [40] = "Forty",
+                [50] = "Fifty",
+                [60] = "Sixty",
+                [70] = "Seventy",
+                [80] = "Eighty",
+                [90] = "Ninety",
+                [100] = "Hundred",
+                [1000] = "Thousand",
+                [10000000] = "Million",
+                [1000000000] = "Billion"
+            };
+
+            if (input == 0)
+                return "Zero";
+
+            if (input < 21)
+            {
+                return map[input];
+            }
+
+            if (input < 100)
+            {
+                var inputWithoutRemainder = input / 10;
+                var remainder = input % 10;
+                if (remainder > 0)
+                {
+                    return map[inputWithoutRemainder * 10] + " " + map[remainder];
+                }
+                else
+                {
+                    return map[inputWithoutRemainder * 10];
+                }
+
+            }
+
+            if (input < 1000)
+            {
+                var inputWithoutRemainder = input / 100;
+                var onlyRemainder = input % 100;
+                if (onlyRemainder > 0)
+                {
+                    return map[inputWithoutRemainder] + " " + "Hundred " + NumberToWords(onlyRemainder);
+                }
+                else
+                {
+                    return map[inputWithoutRemainder] + " " + "Hundred";
+                }
+            }
+
+            if (input < 1000000)
+            {
+                var inputWithoutRemainder = input / 1000;
+                var onlyRemainder = input % 1000;
+                if (onlyRemainder > 0)
+                {
+                    return NumberToWords(inputWithoutRemainder) + " " + "Thousand " + NumberToWords(onlyRemainder);
+                }
+                else
+                {
+                    return NumberToWords(inputWithoutRemainder) + " " + "Thousand";
+                }
+            }
+
+            if (input < 1000000000)
+            {
+                var inputWithoutRemainder = input / 1000000;
+                var onlyRemainder = input % 1000000;
+                if (onlyRemainder > 0)
+                {
+                    return NumberToWords(inputWithoutRemainder) + " " + "Million " + NumberToWords(onlyRemainder);
+                }
+                else
+                {
+                    return NumberToWords(inputWithoutRemainder) + " " + "Million";
+                }
+            }
+
+            else
+            {
+                var inputWithoutRemainder = input / 1000000000;
+                var onlyRemainder = input % 1000000000;
+                if (onlyRemainder > 0)
+                {
+                    return NumberToWords(inputWithoutRemainder) + " " + "Billion " + NumberToWords(onlyRemainder);
+                }
+                else
+                {
+                    return NumberToWords(inputWithoutRemainder) + " " + "Billion";
+                }
+            }
+
+            return String.Empty;
+        }
+    }
+}
