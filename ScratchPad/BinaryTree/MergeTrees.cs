ddba1a40b3a01051044d80c2b1404a31c8e63328
Kanishka Garg <kanishka.garg@insureon.com>
2018-10-12 12:07:07 -0500

    Add code to merge two binary tree; recursively

---
+++ ScratchPad/BinaryTree/MergeTrees.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScratchPadTests.BinaryTree;
+
+namespace ScratchPad.BinaryTree
+{
+    public class MergeTrees
+    {
+        public TreeNode MergeTwoTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null)
+                return t2;
+
+            if (t2 == null)
+                return t1;
+
+            var runner1 = t1;
+            var runner2 = t2;
+
+            runner1.data += runner2.data;
+
+            runner1.left = MergeTwoTrees(runner1.left, runner2.left);
+            runner1.right = MergeTwoTrees(runner1.right, runner2.right);
+
+            return t1;
+        }
+    }
+}
