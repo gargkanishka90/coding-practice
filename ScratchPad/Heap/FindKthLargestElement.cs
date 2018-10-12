7272a6f7a466affa19e076199daf6437d591e7af
Kanishka Garg <kanishkagarg@Kanishkas-MacBook-Pro.local>
2018-10-12 00:37:56 -0500

    Find Kth Largest Element in an Array and its unit test

---
+++ ScratchPad/Heap/FindKthLargestElement.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScratchPad.Heap
+{
+    public class FindKthLargestElement
+    {
+        public int Find(int[] nums, int k)
+        {
+            var minHeap = new MinHeap(k);
+
+            foreach (var num in nums)
+            {
+                minHeap.Insert(num);
+            }
+
+            return minHeap.PeekMin();
+        }
+
+        public class MinHeap
+        {
+            public IList<int> _data;
+            public int size;
+
+            public MinHeap(int k)
+            {
+                _data = new List<int>();
+                size = k;
+            }
+
+            public void Insert(int key)
+            {
+                if (_data.Count < size)
+                {
+                    _data.Add(key);
+                    HeapifyUp(_data.Count - 1);
+                }
+                else
+                {
+                    if (key > PeekMin())
+                    {
+                        _data[0] = key;
+                        HeapifyDown(0);
+                    }
+                }
+            }
+
+            public void HeapifyUp(int i)
+            {
+
+                while (i > 0)
+                {
+                    var parent = (i - 1) / 2;
+                    if (_data[parent] > _data[i])
+                    {
+                        var temp = _data[parent];
+                        _data[parent] = _data[i];
+                        _data[i] = temp;
+                        i = parent;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            public void HeapifyDown(int i)
+            {
+                var left = 2 * i + 1;
+                var right = 2 * i + 2;
+                var min = i;
+
+                if (left < _data.Count && _data[left] < _data[min])
+                {
+                    min = left;
+                }
+
+                if (right < _data.Count && _data[right] < _data[min])
+                {
+                    min = right;
+                }
+
+                if (min != i)
+                {
+                    var temp = _data[i];
+                    _data[i] = _data[min];
+                    _data[min] = temp;
+                    HeapifyDown(min);
+                }
+            }
+
+            public int PeekMin()
+            {
+                return _data[0];
+            }
+        }
+    }
+}
---
+++ ScratchPadTests/Tests/Heap/FindKthLargestElementTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using ScratchPad.Heap;
+
+namespace ScratchPadTests.Tests.Heap
+{
+    [TestFixture]
+    public class FindKthLargestElementTests
+    {
+        [Test]
+        public void Test01()
+        {
+            var instance = new FindKthLargestElement();
+            Assert.AreEqual(5, instance.Find(new[] { 3, 2, 1, 5, 6, 4 }, 2));
+            Assert.AreEqual(4, instance.Find(new[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 }, 4));
+        }
+    }
+}
