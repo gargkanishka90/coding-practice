5e031cf6488e6e8df4dbcda598ade26c94c1046d
Kanishka Garg <kanishkagarg@Kanishkas-MacBook-Pro.local>
2018-10-12 23:51:37 -0500

    add word break problem

---
+++ ScratchPadTests/Tests/Leetcode/WordBreakProblemTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using ScratchPad.Leetcode;
+
+namespace ScratchPadTests.Tests.Leetcode
+{
+    [TestFixture]
+    public class WordBreakProblemTests
+    {
+        [Test]
+        public void Test_Memoization(){
+            var ins = new WordBreakProblem();
+            var word = "ilikesamsung";
+            var dict = new[] { "i", "like", "samsung", "sam" };
+            var resr = ins.WordBreak(word, dict);
+            Assert.IsTrue(resr);
+        }
+    }
+}
